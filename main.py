f67109a1fd4d2d1705d59544fa70505c40d99bbf
Andres Reinoza <77939125+andyreinozaa@users.noreply.github.com>
2023-07-06 19:49:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 """
 Ejercicio Bola de Cristal
 
-a. Una `BolaDeCristal` es un artefacto que te permite ver cuál será tu próxima mascota al enviarle el mensaje `adivinar_el_futuro`, utilizando la api: [https://dog.ceo/api/breeds/image/random](https://dog.ceo/api/breeds/image/random) 
+Una `BolaDeCristal` es un artefacto que te permite ver cuál será tu próxima mascota al enviarle el mensaje `adivinar_el_futuro`, utilizando la api: [https://dog.ceo/api/breeds/image/random](https://dog.ceo/api/breeds/image/random) 
 Definí los atributos y métodos necesarios para que el objeto se comporte como se dijo anteriormente.
 
-b. ¿Cuál es el dominio al que estamos consultando?
-
-c. Explicar cómo harías para saber cuántas razas de perro almacena la API.
-
-d. Describí la arquitectura cliente-servidor y los roles de cada parte.
 """
 
 import requests 
